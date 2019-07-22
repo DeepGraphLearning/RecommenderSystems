54c25c4323ba058091174f984058ab7e8d757730
Chence Shi <chenceshi@pku.edu.cn>
2019-07-22 13:52:28 -0400

    change batch_size to 1024

--- featureRec/run_joint_criteo.sh
+++ featureRec/run_joint_criteo.sh
@@ -5,5 +5,5 @@ CUDA_VISIBLE_DEVICES=0 python -m autoint.train \
                        --save_path ./models/Criteo/b3h2_400x2_64x64x64/ \
                        --field_size 39  --run_times 1 \
                        --deep_layers "[400, 400]" --dropout_keep_prob "[1, 1, 1]" \
-                       --epoch 3 --batch_size 128 \
+                       --epoch 3 --batch_size 1024 \
 
